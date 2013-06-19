bf14a05ffe26663b0c279b651261cc6aee0ca24e
schneems <richard.schneeman@gmail.com>
2013-06-19 14:27:46 -0500

    No ./bin test
    Have to run to the airport now. Not 100% sure if this is a valid test :(
    
    Do not merge, but you can use to test in the mean time if you need. Will try to verify the validity of test before flight takes off. Will update branch and notify.

--- spec/rails4_spec.rb
+++ spec/rails4_spec.rb
@@ -8,4 +8,14 @@ describe "Rails 4.x" do
     end
   end
 
+  it "missing ./bin still works" do
+    Hatchet::AnvilApp.new("rails4-manifest").deploy do |app, heroku|
+      add_database(app, heroku)
+      expect(app.output).to match("Ruby/Rails")
+      app.run("console") do |console|
+        result = console.run("puts 'hello world'")
+        expect(result).to match('hello world')
+      end
+    end
+  end
 end
